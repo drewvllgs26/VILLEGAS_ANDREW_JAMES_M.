17e3231a8cf713a1e14ade29ede1b479fd703648
Andrew James Villegas <andrewvillegas610@gmail.com>
2024-03-27 01:11:20 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'act5_villegas',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
